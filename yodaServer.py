ebbac10b35d3bc0ee74adbb59d1a26860d9d1971
swq2101 <69648780+swq2101@users.noreply.github.com>
2020-12-23 18:27:17 -0500

    Started making flask server to host simulation

---
+++ yodaServer.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('my_form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == "__main__":
+    # Launch the Flask dev server
+    app.run(host="localhost", debug=True)
