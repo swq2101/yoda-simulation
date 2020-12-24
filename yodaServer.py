4a77134bff1811563fb5aa34b0b67329d0978c15
swq2101 <69648780+swq2101@users.noreply.github.com>
2020-12-24 17:05:19 -0500

    Added method that parses form for data and returns the current population

--- yodaServer.py
+++ yodaServer.py
@@ -1,10 +1,37 @@
 from flask import Flask, request, render_template
+import yoda_population_simulation
 
 app = Flask(__name__)
 
+currentPopulation = 0
+frogsAvailable = 0
+mushroomsHarvested = 0
+
 @app.route('/')
-def home_page():
+def submit_form():
     return render_template('yodaForm.html')
+
+
+@app.route('/results', methods=['POST'])
+def load_form():
+    yoda_population_simulation.startingPopulation = int(request.form['startingPopulation'])
+    frogsAvalable = int(request.form['frogsHarvested'])
+    mushroomsAvailable = int(request.form['mushroomsHarvested'])
+
+    for year in range(0, yoda_population_simulation.millenium):
+        currentPopulation = yoda_population_simulation.runYear(yoda_population_simulation.frogsHarvested, yoda_population_simulation.mushroomsHarvested, mushroomsAvailable, frogsAvailable, yoda_population_simulation.fertilityx, yoda_population_simulation.fertilityy, yoda_population_simulation.infantMortality, yoda_population_simulation.deathStarChance)
+        print(str(currentPopulation))
+    
+    templateData = {
+        'starting_population': yoda_population_simulation.startingPopulation,
+        'current_population': currentPopulation,
+        'frogs_available': frogsAvailable,
+        'mushrooms_available': mushroomsAvailable
+    }   
+
+    return render_template('yodaResult.html', **templateData)
+
+
 
 if __name__ == "__main__":
     # Launch the Flask dev server
