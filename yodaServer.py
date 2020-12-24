d4601fbf600892941f8c05569ed0cf84e9060289
swq2101 <69648780+swq2101@users.noreply.github.com>
2020-12-24 14:55:39 -0500

    Load home page done

--- yodaServer.py
+++ yodaServer.py
@@ -3,14 +3,8 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def my_form():
-    return render_template('my_form.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+def home_page():
+    return render_template('yodaForm.html')
 
 if __name__ == "__main__":
     # Launch the Flask dev server
